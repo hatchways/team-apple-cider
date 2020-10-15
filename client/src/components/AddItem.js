f833b0ad44313a36e09e25f1262e29fefdb244d1
Sam <samuel_morgan_@outlook.com>
2020-10-15 23:29:34 +0100

    Converted additems from css to mui

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -8,25 +8,35 @@ import {
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-import "css/AddItem.css";
-
 const useStyles = makeStyles((theme) => ({
-  //
+  dashboardAddItem: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "2rem",
+  },
+  addItemInput: {
+    display: "flex",
+    flexDirection: "row",
+  },
 }));
 
 const AddItem = () => {
   const classes = useStyles();
   return (
-    <div className="dashboard-add-item">
+    <div className={classes.dashboardAddItem}>
       <Typography variant="h4">Add new item:</Typography>
-      <div className="add-item-input">
+      <div className={classes.addItemInput}>
         <TextField label="Please enter your link here" />
         <Select>
           <MenuItem value={"clothes"}>Clothes</MenuItem>
           <MenuItem value={"furniture"}>Furniture</MenuItem>
           <MenuItem value={"luxury"}>Luxury</MenuItem>
         </Select>
-        <Button variant="contained">Default</Button>
+        <Button className={classes.addButton} variant="contained">
+          Default
+        </Button>
       </div>
     </div>
   );
