116762fa21f6220d29095019f779bdfe6319c954
Sam <samuel_morgan_@outlook.com>
2020-10-15 15:20:10 +0100

    Added search without styling

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -1,6 +1,12 @@
 import React from "react";
 
-import { Typography } from "@material-ui/core";
+import {
+  Typography,
+  TextField,
+  Select,
+  MenuItem,
+  Button,
+} from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 
 import "css/AddItem.css";
@@ -13,6 +19,15 @@ const AddItem = () => {
   return (
     <div className="dashboard-add-item">
       <Typography variant="h4">Add new item:</Typography>
+      <div className="add-item-input">
+        <TextField label="Please enter your link here" />
+        <Select>
+          <MenuItem value={"clothes"}>Clothes</MenuItem>
+          <MenuItem value={"furniture"}>Furniture</MenuItem>
+          <MenuItem value={"luxury"}>Luxury</MenuItem>
+        </Select>
+        <Button variant="contained">Default</Button>
+      </div>
     </div>
   );
 };
