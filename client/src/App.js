733c86eb61e6c1c4aa2820f73adbf293d99d9818
Sam <samuel_morgan_@outlook.com>
2020-10-15 18:10:45 +0100

    Removed app/index css

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,6 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { theme } from "themes/theme";
 import DashboardPage from "pages/Dashboard";
 
-import "css/App.css";
-
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
