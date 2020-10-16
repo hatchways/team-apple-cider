d0e9648f91a343cc4bd46130ea2e1130ba72e45e
Sam <samuel_morgan_@outlook.com>
2020-10-16 10:25:07 +0100

    Dropdown working

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-  Container,
+  Box,
   Typography,
   TextField,
   Select,
@@ -14,13 +14,16 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    padding: "2rem",
+    margin: "2rem",
   },
   addItemInput: {
     display: "flex",
-    margin: "2rem",
     flexDirection: "row",
     justifyContent: "center",
+    backgroundColor: "white",
+    margin: "2rem",
+    padding: "1rem",
+    borderRadius: "10rem",
   },
   addButton: {
     borderRadius: "10rem",
@@ -32,18 +35,26 @@ const useStyles = makeStyles((theme) => ({
     width: "20rem",
   },
   dropdownList: {
-    width: "20rem",
+    width: "10rem",
   },
 }));
 
 const AddItem = () => {
+  const [selectedItem, setSelectedItem] = useState("none");
   const classes = useStyles();
   return (
-    <Container className={classes.dashboardAddItem}>
+    <Box className={classes.dashboardAddItem}>
       <Typography variant="h4">Add new item:</Typography>
-      <Container className={classes.addItemInput}>
+      <Box className={classes.addItemInput}>
         <TextField className={classes.linkForm} label="Paste your link here" />
-        <Select className={classes.dropdownList}>
+        <Select
+          className={classes.dropdownList}
+          value={selectedItem}
+          onChange={(e) => setSelectedItem(e.target.value)}
+        >
+          <MenuItem value="none" disabled>
+            Select List
+          </MenuItem>
           <MenuItem value={"clothes"}>Clothes</MenuItem>
           <MenuItem value={"furniture"}>Furniture</MenuItem>
           <MenuItem value={"luxury"}>Luxury</MenuItem>
@@ -51,8 +62,8 @@ const AddItem = () => {
         <Button className={classes.addButton} variant="contained">
           ADD
         </Button>
-      </Container>
-    </Container>
+      </Box>
+    </Box>
   );
 };
 
