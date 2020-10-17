5543ca1895ceaa446353826c8bbf5e79db92d636
Sam <samuel_morgan_@outlook.com>
2020-10-17 22:12:08 +0100

    Opens amazon

--- server/scraper.py
+++ server/scraper.py
@@ -1,2 +1,8 @@
-import selenium
-print('hi')
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+PATH = '/home/user/scraping/geckodriver'
+browser = webdriver.Firefox(executable_path=PATH)
+browser.get('http://www.amazon.com')
+time.sleep(5) 
+browser.quit()
