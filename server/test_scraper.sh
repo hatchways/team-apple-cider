ffc8684f4ece529429a7489f11aa910cb7aaad3f
Sam <samuel_morgan_@outlook.com>
2020-10-18 22:02:26 +0100

    Bash script uses bash array

--- server/test_scraper.sh
+++ server/test_scraper.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 singleURL="https://www.amazon.co.uk/dp/B07PJV3JPR"
-urlArray="['https://www.amazon.co.uk/dp/B07PJV3JPR','https://www.amazon.co.uk/dp/B07WMKZZXL','https://www.amazon.co.uk/dp/B08H95Y452']"
+multipleURLs=("https://www.amazon.co.uk/dp/B07PJV3JPR" "https://www.amazon.co.uk/dp/B07WMKZZXL" "https://www.amazon.co.uk/dp/B08H95Y452")
+
+urlArray=$(printf '%s\n' "${multipleURLs[@]}" | jq -R . | jq -s . | tr -d '\040\011\012\015')
 
 echo -e '\nSingle item using string input:\n'
 python3 -c "import scraper; item = scraper.scrapeAmazon('${singleURL}'); print(item)"
 echo -e '\nMultiple items using array input:\n'
 python3 -c "import scraper; items = scraper.scrapeAmazon(${urlArray}); [print(item) for item in items]"
- 
-
-
