3a2a94ece91aacca75d06e29aa2f1eab0ce814f4
Sam <samuel_morgan_@outlook.com>
2020-10-18 16:13:03 +0100

    All sep functions

--- server/scraper.py
+++ server/scraper.py
@@ -8,30 +8,39 @@ options.add_argument("--headless")
 PATH = '/home/user/scraping/geckodriver'
 browser = webdriver.Firefox(executable_path=PATH, options=options)
 
-def getAvailability(URL):
-    text = browser.find_element_by_css_selector("#availability > *:first-child").text
-    return (text == 'In stock.')
-    # Needs to decide if "In stock on x date" counts as available
+def getTitle():
+    try: return browser.find_element_by_id('productTitle').text
+    except: return None
+
+def getOldPrice():
+    try: return browser.find_element_by_class_name('priceBlockStrikePriceString').text
+    except: return None
+
+def getPrice():
+    try: return browser.find_element_by_id('priceblock_ourprice').text
+    except: return None
+
+def getImgURL():
+    try: return browser.find_element_by_id('landingImage').get_attribute("src")
+    except: return None
+
+def getAvailability():
+    try:
+        text = browser.find_element_by_css_selector("#availability > *:first-child").text
+        return (text == 'In stock.')
+        # Needs to decide if "In stock on x date" counts as available
+    except: return None
+
 
 def getItem(URL):
     browser.get(URL)
     details = {
-        "title": None,
-        "oldPrice": None,
-        "price": None,
-        "imgURL": None,
-        "available": None,
-    }
-    try: details["title"] = browser.find_element_by_id('productTitle').text
-    except: pass
-    try: details["oldPrice"] = browser.find_element_by_class_name('priceBlockStrikePriceString').text
-    except: pass
-    try: details["price"] = browser.find_element_by_id('priceblock_ourprice').text
-    except: pass
-    try: details["imgURL"] = browser.find_element_by_id('landingImage').get_attribute("src")
-    except: pass    
-    try: details["available"] = getAvailability(URL)
-    except: pass    
+        "title": getTitle(),
+        "oldPrice": getOldPrice(),
+        "price": getPrice(),
+        "imgURL": getImgURL(),
+        "available": getAvailability(),
+    }    
     print(details)
 
 try:
