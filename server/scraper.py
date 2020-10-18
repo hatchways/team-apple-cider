92123844ccd2831698e3e127e189883e72d6c0ce
Sam <samuel_morgan_@outlook.com>
2020-10-18 15:32:15 +0100

    Can get multiple items, without sale prices

--- server/scraper.py
+++ server/scraper.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 
 options = Options()
@@ -6,14 +7,21 @@ options.add_argument("--headless")
 
 PATH = '/home/user/scraping/geckodriver'
 browser = webdriver.Firefox(executable_path=PATH, options=options)
-browser.get('https://www.amazon.co.uk/dp/B07PJV3JPR/')
+print('\n')
 
-try:
+def getItem(URL):
+    browser.get(URL)
     title = browser.find_element_by_id('productTitle').text
-    oldPrice = browser.find_element_by_class_name('priceBlockStrikePriceString').text
+    try: oldPrice = browser.find_element_by_class_name('priceBlockStrikePriceString').text
+    except: oldPrice = None
     price = browser.find_element_by_id('priceblock_ourprice').text
     print('Product Title: ' + title)
-    print('Old Price: ' + oldPrice)
-    print('Price: ' + price)
+    if (oldPrice): print('Old Price: ' + oldPrice)
+    print('Price: ' + price + '\n')
+
+try:
+    getItem('https://www.amazon.co.uk/dp/B07PJV3JPR')
+    getItem('https://www.amazon.co.uk/dp/B07WMKZZXL')
 finally:
     browser.quit()
+
