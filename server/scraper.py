a28ae957f8decabc02cc8d248cb7bd09e7657063
Sam <samuel_morgan_@outlook.com>
2020-10-18 15:41:02 +0100

    Details stored as object

--- server/scraper.py
+++ server/scraper.py
@@ -7,19 +7,21 @@ options.add_argument("--headless")
 
 PATH = '/home/user/scraping/geckodriver'
 browser = webdriver.Firefox(executable_path=PATH, options=options)
-print('\n')
 
 def getItem(URL):
     browser.get(URL)
-    try: title = browser.find_element_by_id('productTitle').text
-    except: title = None
-    try: oldPrice = browser.find_element_by_class_name('priceBlockStrikePriceString').text
-    except: oldPrice = None
-    try: price = browser.find_element_by_id('priceblock_ourprice').text
-    except: price = None
-    if (title): print('Product Title: ' + title)
-    if (oldPrice): print('Old Price: ' + oldPrice)
-    if (price):print('Price: ' + price + '\n')
+    details = {
+        "title": None,
+        "oldPrice": None,
+        "price": None,
+    }
+    try: details["title"] = browser.find_element_by_id('productTitle').text
+    except: pass
+    try: details["oldPrice"] = browser.find_element_by_class_name('priceBlockStrikePriceString').text
+    except: pass
+    try: details["price"] = browser.find_element_by_id('priceblock_ourprice').text
+    except: pass
+    print(details)
 
 try:
     getItem('https://www.amazon.co.uk/dp/B07PJV3JPR')
