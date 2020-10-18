0dc9502c22002736428f7b7dda1a672cd1aee992
Sam <samuel_morgan_@outlook.com>
2020-10-18 15:15:40 +0100

    Scraper now headless

--- server/scraper.py
+++ server/scraper.py
@@ -1,9 +1,14 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.add_argument("--headless")
 
 PATH = '/home/user/scraping/geckodriver'
-browser = webdriver.Firefox(executable_path=PATH)
+browser = webdriver.Firefox(executable_path=PATH, options=options)
 browser.get('https://www.amazon.co.uk/dp/B07PJV3JPR/')
+
 try:
     price = browser.find_element_by_id('priceblock_ourprice').text
     print(price)
