2d295e6794bb90cdd63e681bcda05e5b057bc6d7
Sam <samuel_morgan_@outlook.com>
2020-10-18 17:41:22 +0100

    singleURL & urlArray vars, newlines

--- server/test_scraper.sh
+++ server/test_scraper.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo 'Single item using string input:'
-python3 -c "import scraper; item = scraper.scrapeAmazon('https://www.amazon.co.uk/dp/B07PJV3JPR'); print(item)"
-echo -e "\n"
-echo 'Multiple items using array input:'
-python3 -c "import scraper; items = scraper.scrapeAmazon(['https://www.amazon.co.uk/dp/B07PJV3JPR','https://www.amazon.co.uk/dp/B07WMKZZXL','https://www.amazon.co.uk/dp/B08H95Y452']); [print(item) for item in items]"
+singleURL="https://www.amazon.co.uk/dp/B07PJV3JPR"
+urlArray="['https://www.amazon.co.uk/dp/B07PJV3JPR','https://www.amazon.co.uk/dp/B07WMKZZXL','https://www.amazon.co.uk/dp/B08H95Y452']"
+
+echo -e '\nSingle item using string input:\n'
+python3 -c "import scraper; item = scraper.scrapeAmazon('${singleURL}'); print(item)"
+echo -e '\nMultiple items using array input:\n'
+python3 -c "import scraper; items = scraper.scrapeAmazon(${urlArray}); [print(item) for item in items]"
  
 
 
