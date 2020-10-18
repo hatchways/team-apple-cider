9856eb0630dea5f94a58202027fa78f0a5545f1c
Sam <samuel_morgan_@outlook.com>
2020-10-18 15:23:32 +0100

    Title & oldPrice added

--- server/scraper.py
+++ server/scraper.py
@@ -9,7 +9,11 @@ browser = webdriver.Firefox(executable_path=PATH, options=options)
 browser.get('https://www.amazon.co.uk/dp/B07PJV3JPR/')
 
 try:
+    title = browser.find_element_by_id('productTitle').text
+    oldPrice = browser.find_element_by_class_name('priceBlockStrikePriceString').text
     price = browser.find_element_by_id('priceblock_ourprice').text
-    print(price)
+    print('Product Title: ' + title)
+    print('Old Price: ' + oldPrice)
+    print('Price: ' + price)
 finally:
     browser.quit()
