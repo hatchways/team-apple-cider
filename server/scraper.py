2ca168d36f4d436a9ad3780856ad3d1d57e67a9f
Sam <samuel_morgan_@outlook.com>
2020-10-18 16:01:41 +0100

    getAvailability fn

--- server/scraper.py
+++ server/scraper.py
@@ -8,6 +8,11 @@ options.add_argument("--headless")
 PATH = '/home/user/scraping/geckodriver'
 browser = webdriver.Firefox(executable_path=PATH, options=options)
 
+def getAvailability(URL):
+    text = browser.find_element_by_css_selector("#availability > *:first-child").text
+    return (text == 'In stock.')
+    # Needs to decide if "In stock on x date" counts as available
+
 def getItem(URL):
     browser.get(URL)
     details = {
@@ -15,6 +20,7 @@ def getItem(URL):
         "oldPrice": None,
         "price": None,
         "imgURL": None,
+        "available": None,
     }
     try: details["title"] = browser.find_element_by_id('productTitle').text
     except: pass
@@ -24,11 +30,14 @@ def getItem(URL):
     except: pass
     try: details["imgURL"] = browser.find_element_by_id('landingImage').get_attribute("src")
     except: pass    
+    try: details["available"] = getAvailability(URL)
+    except: pass    
     print(details)
 
 try:
     getItem('https://www.amazon.co.uk/dp/B07PJV3JPR')
     getItem('https://www.amazon.co.uk/dp/B07WMKZZXL')
+    getItem('https://www.amazon.co.uk/dp/B08H95Y452')
 finally:
     browser.quit()
 
