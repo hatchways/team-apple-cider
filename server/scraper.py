3a9748be2ea74b31b300f05aee3cd660e04f6cd6
Sam <samuel_morgan_@outlook.com>
2020-10-18 15:13:51 +0100

    Prints price of item

--- server/scraper.py
+++ server/scraper.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 PATH = '/home/user/scraping/geckodriver'
 browser = webdriver.Firefox(executable_path=PATH)
-browser.get('http://www.amazon.com')
-time.sleep(5) 
-browser.quit()
+browser.get('https://www.amazon.co.uk/dp/B07PJV3JPR/')
+try:
+    price = browser.find_element_by_id('priceblock_ourprice').text
+    print(price)
+finally:
+    browser.quit()
