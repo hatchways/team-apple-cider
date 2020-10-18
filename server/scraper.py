4dd862f156e4c11c714aa3def9dc697504600c00
Sam <samuel_morgan_@outlook.com>
2020-10-18 15:34:36 +0100

    Added error checking to all params

--- server/scraper.py
+++ server/scraper.py
@@ -11,13 +11,15 @@ print('\n')
 
 def getItem(URL):
     browser.get(URL)
-    title = browser.find_element_by_id('productTitle').text
+    try: title = browser.find_element_by_id('productTitle').text
+    except: title = None
     try: oldPrice = browser.find_element_by_class_name('priceBlockStrikePriceString').text
     except: oldPrice = None
-    price = browser.find_element_by_id('priceblock_ourprice').text
-    print('Product Title: ' + title)
+    try: price = browser.find_element_by_id('priceblock_ourprice').text
+    except: price = None
+    if (title): print('Product Title: ' + title)
     if (oldPrice): print('Old Price: ' + oldPrice)
-    print('Price: ' + price + '\n')
+    if (price):print('Price: ' + price + '\n')
 
 try:
     getItem('https://www.amazon.co.uk/dp/B07PJV3JPR')
