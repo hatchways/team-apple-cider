8a8d380b64b6c9b84271d287445d4def89d02d83
Sam <samuel_morgan_@outlook.com>
2020-10-20 13:40:36 +0100

    Replaced boxes with Typography

--- client/src/components/ItemDisplay.js
+++ client/src/components/ItemDisplay.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, Link } from "@material-ui/core";
+import { Box, Link, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   itemContainer: {
@@ -36,9 +36,11 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(0.25),
   },
   itemOldPrice: {
+    display: "inline",
     textDecoration: "line-through",
   },
   itemPrice: {
+    display: "inline",
     color: "red",
     fontWeight: "bold",
   },
@@ -52,19 +54,15 @@ const ItemDisplay = (props) => {
     <Box className={classes.itemContainer}>
       <img className={classes.itemImage} src={item.imgURL} alt={item.title} />
       <Box className={classes.itemTextContainer}>
-        <Box component="div" className={classes.itemTitle}>
-          {item.title}
-        </Box>
+        <Typography className={classes.itemTitle}>{item.title}</Typography>
         <Link href={item.shopURL} className={classes.shopURL}>
           {item.shopURL}
         </Link>
         <Box className={classes.priceTextContainer}>
-          <Box component="span" className={classes.itemOldPrice}>
+          <Typography className={classes.itemOldPrice}>
             {item.oldPrice}
-          </Box>{" "}
-          <Box component="span" className={classes.itemPrice}>
-            {item.price}
-          </Box>
+          </Typography>{" "}
+          <Typography className={classes.itemPrice}>{item.price}</Typography>
         </Box>
       </Box>
     </Box>
