7b706cf3d47cea4951a4f454dac6bf77208a17bb
Mila Ashtari <mashtari1991@gmail.com>
2020-10-19 22:09:35 -0400

    created consistent styling with dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,28 @@ import { MuiThemeProvider } from "@material-ui/core";
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { BrowserRouter, Route } from "react-router-dom";
 
+// import './App.css'; 
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 
 
-
+import { theme } from "themes/theme";
 import DashboardPage from "pages/Dashboard";
 
 function App() {
   return (
     <Fragment>
-      <CssBaseline></CssBaseline>
-      <BrowserRouter>
-          <Route path="/" component={SignUp} exact/>
-          <Route path="/login" component={Login}/>
-        <Route path="/" component={DashboardPage} />
-      </BrowserRouter>
+      <CssBaseline>
+        <MuiThemeProvider theme={theme}>
+          <BrowserRouter>
+            {/* <Route path="/" component={DashboardPage} /> */}
+            <Route path="/" component={SignUp} exact/>
+            <Route path="/login" component={Login}/>
+          </BrowserRouter>
+        </MuiThemeProvider>
+      </CssBaseline>
     </Fragment>
-      
-  )
+  );
 }
 
 export default App;
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,37 +1,83 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Link, Redirect, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import { Button, TextField, Box, Input } from '@material-ui/core'
 
-import root from '../style/signupLoginPages'
-
-
-const useStyles = makeStyles({
-    root: root
-});
+const useStyles = makeStyles((theme)=>({
+login:{
+    maxWidth:'1200px',
+    margin:'0 auto',
+    height:'100vh',
+    padding:'50px',
+    backgroundColor:'#44475ab9',
+},
+formContainer:{
+    border:' solid rgb(241, 238, 232) 1px',
+    borderRadius:'1%',
+    width:'50% ',
+    maxWidth:'400px',
+    minWidth:'250px',
+    margin: '0 auto',
+    textAlign:'center',
+    backgroundColor:'#f7f3f3',
+    padding:'30px',
+},
+h2:{
+    marginTop:0,
+},
+textField:{
+    display:'block',
+    width:'80%',
+    margin:'5px auto 15px auto',
+    textAlign:'center',
+    backgroundColor:'white'
+},
+button:{
+    marginTop:'25px',
+    backgroundColor:'#DF1B1B'
+},
+signup:{
+    display:'flex',
+    padding:'20px',
+    borderTop:'1px solid rgba(128, 128, 128, 0.274)',
+    justifyContent:'center',
+    marginTop:'40px',
+    textAlign:'center',
+},
+p:{
+    margin:0,
+    marginRight:'10px'
+},
+signupLink:{
+    color:'#f50057'
+}
+}));
 function SignUp (){
     const classes = useStyles();
+    const [userName, setName]=useState('')
+    const [userEmail,setEmail]=useState('')
+    const [userPass, setPass]=useState('')
     return(
-        <section className={classes.root}>
-            <div className='container'>
-                <form >
-                    <h2>Sign Up</h2>
-                    <label>Your name:</label>
-                    <TextField className='textField' variant="outlined" fullWidth label="name" required/>
-                    <label>Your email address:</label>
-                    <TextField className='textField' variant="outlined" fullWidth label="email" required type="email"/>
-                    <label>Password:</label>
-                    <TextField className='textField' variant="outlined" fullWidth label="password" required type="password"/>
-                    <Button variant="contained" color="secondary">Create an account</Button>
-                </form>
-                <div className='login'>
-                    <p>Already have an account?</p>
-                    <Link  className='loginLink' to="/login">login</Link>
-                </div>
-            </div>
-        </section>
+            <section className={classes.login}>
+                <Box className={classes.formContainer}>
+                    <form>
+                        <h2 className={classes.h2}>Sign up</h2>
+                        <label>Your Name</label>
+                        <TextField className={classes.textField} variant="outlined" label="name"  fullWidth required type="text"  onChange={(e)=>setName(e.target.value)}/>
+                        <label>Your email address:</label>
+                        <TextField className={classes.textField} variant="outlined" label="email"  fullWidth required type="email"  onChange={(e)=>setEmail(e.target.value)}/>
+                        <label>Password:</label>
+                        <TextField className={classes.textField} variant="outlined" label="password" fullWidth required type="password" onChange={(e)=>setPass(e.target.value)}/>
+                        <Button className={classes.button} variant="contained" color="secondary" >Login</Button>
+                    </form>
+                    <Box className={classes.signup}>
+                        <p className={classes.p}>Don't have an account?</p>
+                        <Link className={classes.signupLink} to="/login">Create an account</Link>
+                    </Box>
+                </Box>
+            </section>
     )
 }
+
 
 export default SignUp
