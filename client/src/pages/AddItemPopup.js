6dfe99ea434eddbfdf052ceb4e5794e39860bfb0
Sam <samuel_morgan_@outlook.com>
2020-10-21 22:00:12 +0100

    Changed padding of paper

--- client/src/pages/AddItemPopup.js
+++ client/src/pages/AddItemPopup.js
@@ -16,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
+    padding: theme.spacing(4),
     borderRadius: "0.3rem",
     overflow: "hidden",
   },
