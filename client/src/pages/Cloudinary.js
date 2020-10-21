6eaaf6299ef0806ccf1c81b93a817d8cf3ed1c3e
adams <adamsliu97@gmail.com>
2020-10-21 19:20:28 -0400

    Update Cloudinary.js

--- client/src/pages/Cloudinary.js
+++ client/src/pages/Cloudinary.js
@@ -2,8 +2,28 @@ import React, { useState } from "react";
 import { Box, Typography } from "@material-ui/core";
 
 const Cloudinary = () => {
-  const [image, setImage] = useSate("");
+  const [image, setImage] = useState("");
   const [loading, setLoading] = useState(false);
+
+  const uploadImage = async e =>{
+    const files = e.target.files
+    const data = new FormData()
+    data.append('file',files[0])
+    data.append('upload_preset', 'applecider')
+    setLoading(true)
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/applecider/image/upload',
+      {
+        method: 'POST',
+        body: data
+      }
+    )
+      const file = await res.json()
+
+      setImage(file.secure_url)
+      setLoading(false)
+  }
+
   return (
     <Box>
       <Typography>Hello World!</Typography>
@@ -11,12 +31,19 @@ const Cloudinary = () => {
         <input
           type="file"
           name="file"
-          placeholder="uplaod na image"
+          placeholder="upload an image"
           onChange={uploadImage}
         />
+        {loading?(
+          <h3>Loading...</h3>
+        ): (
+          <img src={image} style={{width:'300px'}} />
+        )}
       </Box>
     </Box>
   );
 };
 
 export default Cloudinary;
+
+
