a6e4bb76caa19c8aa0973c98d822e07651d8fede
Sam <samuel_morgan_@outlook.com>
2020-10-21 16:38:28 +0100

    Options inside scrape class

--- server/scraper.py
+++ server/scraper.py
@@ -1,20 +1,19 @@
 import re
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
-from selenium.webdriver.chrome.options import Options
-
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--ignore-certificate-errors')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--remote-debugging-port=9222')
-executable_path=ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
 
 
 class ScrapeAmazon:   
     def __init__(self, URL):
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--remote-debugging-port=9222')
+        executable_path=ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
         self.driver = webdriver.Chrome(executable_path=executable_path, options=chrome_options)  
         self.driver.get(URL)
         self.itemURL = URL
