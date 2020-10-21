b0db43f4e8d81d8763335f48f91a271a08f36cf2
Sam <samuel_morgan_@outlook.com>
2020-10-21 19:26:10 +0100

    Returns predefined item from /scrape route

--- server/api/scrape_handler.py
+++ server/api/scrape_handler.py
@@ -1,6 +1,8 @@
 from flask import jsonify, Blueprint
 scrape_handler = Blueprint('scrape_handler', __name__)
+from .scraper import ScrapeAmazon
 
 @scrape_handler.route('/scrape')
-def welcome():
-    return jsonify({'welcomeMessage': 'Route for scraping an Amazon URL'})
+def scrape():
+    item = ScrapeAmazon('https://www.amazon.com/dp/B07YMJ9WFN')
+    return jsonify(item.__dict__)
