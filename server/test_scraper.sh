a536cdc7b7e34cd0d4fb0bdd5b9ba3606342915f
Sam <samuel_morgan_@outlook.com>
2020-10-21 18:57:23 +0100

    Fixed test script typo

--- server/test_scraper.sh
+++ server/test_scraper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo -e '\Test ScrapeAmazon:\n'
+echo -e '\nTest ScrapeAmazon:\n'
 python3 -c "import scraper; item = scraper.ScrapeAmazon('https://www.amazon.com/dp/B07YMJ9WFN'); item2 = scraper.ScrapeAmazon('https://www.amazon.com/dp/B07FCMKK5X'); print(item); print(item2)"
