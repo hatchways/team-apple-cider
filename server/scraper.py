429c9ec6c43ddb80c17588c8e4dfbea7a2475e37
Sam <samuel_morgan_@outlook.com>
2020-10-21 12:51:26 +0100

    Converted scraper to use classes

--- server/scraper.py
+++ server/scraper.py
@@ -7,53 +7,57 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--ignore-certificate-errors')
-chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--remote-debugging-port=9222')
 executable_path=ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
 driver = webdriver.Chrome(executable_path=executable_path, options=chrome_options)
 
-def getTitle():
-    try: return driver.find_element_by_id('productTitle').text
-    except: return None
 
-def getOldPrice():
-    try: return driver.find_element_by_class_name('priceBlockStrikePriceString').text
-    except: return None
-
-def getPrice():
-    try: return driver.find_element_by_id('priceblock_ourprice').text
-    except: return None
-
-def getImgURL():
-    try: return driver.find_element_by_id('landingImage').get_attribute("src")
-    except: return None
-
-def getAvailability():
-    try:
-        text = driver.find_element_by_css_selector("#availability > *:first-child").text
-        return (bool(re.search('in stock', text, re.IGNORECASE)))
-    except: return None
-
-
-def getItem(URL):
-    driver.get(URL)
-    details = {
-        "title": getTitle(),
-        "oldPrice": getOldPrice(),
-        "price": getPrice(),
-        "imgURL": getImgURL(),
-        "available": getAvailability(),
-    }    
-    return details
+class Item:   
+    def __init__(self, URL):
+        driver.get(URL)
+        self.title = self.getTitle() 
+        self.oldPrice = self.getOldPrice()  
+        self.price = self.getPrice()  
+        self.imgURL = self.getImgURL()  
+        self.availability = self.getAvailability()  
+    @staticmethod
+    def getTitle():
+        try: 
+            return driver.find_element_by_id('productTitle').text
+        except: return None
+    @staticmethod
+    def getOldPrice():
+        try: 
+            return driver.find_element_by_class_name('priceBlockStrikePriceString').text
+        except: return None
+    @staticmethod
+    def getPrice():
+        try: 
+            return driver.find_element_by_id('priceblock_ourprice').text
+        except: return None
+    @staticmethod
+    def getImgURL():
+        try: 
+            return driver.find_element_by_id('landingImage').get_attribute("src")
+        except: return None
+    @staticmethod
+    def getAvailability():
+        try:
+            text = driver.find_element_by_css_selector("#availability > *:first-child").text
+            return (bool(re.search('in stock', text, re.IGNORECASE)))
+        except: return None
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
 
 def scrapeAmazon(input):
     try:
         if (isinstance(input, list)):
             items = []
-            for url in input: items.append(getItem(url))
+            for url in input: items.append(Item(url))
             return items
-        elif (isinstance(input, str)): return getItem(input)
+        elif (isinstance(input, str)): return Item(input)
         else: return 'ERROR: INVALID INPUT'
     except:
         return 'ERROR: SCRAPE FAILED'
