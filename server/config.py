8e8e8e296e641542ec1224d56759f978e2b20005
adams <adamsliu97@gmail.com>
2020-10-21 22:30:11 -0400

    added keys

--- server/config.py
+++ server/config.py
@@ -1,3 +1,31 @@
 import os
+from dotenv import load_dotenv
+
+# load all the environment variables in the .env file
+load_dotenv(override=True)
 
 TEAM_NAME = os.environ['TEAM_NAME']
+POSTGRES_USER = os.environ['POSTGRES_USER']
+POSTGRES_PW = os.environ['POSTGRES_PW']
+POSTGRES_URL = os.environ['POSTGRES_URL']
+POSTGRES_DB = os.environ['POSTGRES_DB']
+
+
+class BaseConfig:
+    SECRET_KEY = os.getenv("SECRET_KEY", "example_backup_secret_key")
+    DEBUG = False
+    BCRYPT_LOG_ROUNDS = 13
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    # Create connection between python and psql db
+    DATABASE_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+        user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+    BCRYPT_LOG_ROUNDS = 4
+    CLOUDINARY_NAME= os.environ["CLOUDINARY_NAME"]
+    CLOUDINARY_UPLOAD_PRESET_NAME = os.environ["CLOUDINARY_UPLOAD_PRESET_NAME"]
+
