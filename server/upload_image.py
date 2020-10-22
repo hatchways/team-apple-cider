a27aa6b90f42bf3d92052ffb82567821d9bec365
adams <adamsliu97@gmail.com>
2020-10-21 20:14:50 -0400

    updated .env for cloudinary

--- server/upload_image.py
+++ server/upload_image.py
@@ -7,6 +7,22 @@ import cloudinary.api
 #     return 
 
 image = r"C:\Users\adams\Pictures\pet-rock.jpg"
-upload_preset = r"https://api.cloudinary.com/v1_1/applecider"
-result = cloudinary.uploader.unsigned_upload(image, upload_preset)
-print(result)
+upload_preset = r"https://api.cloudinary.com/v1_1/applecider/image/upload"
+name = "applecider"
+
+# cloudinary.config( 
+#   cloud_name = "applecider", 
+#   api_key = "713319321679623", 
+#   api_secret = "dpvCJ7wXZFhBy0wTNUjkRkjXDUs" 
+# )
+
+
+# upload_preset= cloudinary.api.create_upload_preset(
+#   unsigned = True, 
+#   folder = "applecider")
+
+# print(upload_preset)
+
+result = cloudinary.uploader.unsigned_upload(image, "xnxitkef",
+  cloud_name = 'applecider')
+print(result['secure_url'])
