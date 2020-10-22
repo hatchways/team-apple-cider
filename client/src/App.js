69e2041c816c9742e4f46eed48995bcb1689f6d1
adams <adamsliu97@gmail.com>
2020-10-22 12:30:08 -0400

    deleted unesscessary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
 import React, { Fragment } from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from "@material-ui/core/CssBaseline";
 import { BrowserRouter, Route } from "react-router-dom";
 
-// import './App.css'; 
-import SignUp from './components/SignUp';
-import Login from './components/Login';
-
+// import './App.css';
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 
 import { theme } from "themes/theme";
 import DashboardPage from "pages/Dashboard";
-import Cloudinary from "pages/Cloudinary";
 
 function App() {
   return (
@@ -18,9 +16,9 @@ function App() {
       <CssBaseline>
         <MuiThemeProvider theme={theme}>
           <BrowserRouter>
-            {/* <Route path="/" component={DashboardPage} /> */}
-            <Route path="/" component={SignUp} exact/>
-            <Route path="/login" component={Login}/>
+            <Route path="/" component={DashboardPage} />
+            <Route path="/" component={SignUp} exact />
+            <Route path="/login" component={Login} />
           </BrowserRouter>
         </MuiThemeProvider>
       </CssBaseline>
