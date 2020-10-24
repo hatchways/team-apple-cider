b581ea10f3a062e5c26a3279f8f4bad4bc6a786c
Sam <samuel_morgan_@outlook.com>
2020-10-24 19:21:10 +0100

    Fixed broken scraped_items route

--- server/api/scraped_items_handler.py
+++ server/api/scraped_items_handler.py
@@ -2,8 +2,9 @@ import json
 from flask import jsonify, request, Blueprint
 scraped_items_handler = Blueprint('scraped_items_handler', __name__)
 
-@scraped_items_handler.route('/scraped_items')
+@scraped_items_handler.route('/scraped_items', methods = ['GET', 'POST'])
 def scraped_items():
-    return jsonify({'message' : 'Scraped items list'})
+    if request.method == 'GET':
+        return jsonify({'message' : 'Scraped items list'})
 
 
