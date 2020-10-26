f9504f63abe79ae30834309b3ad92676ad1c226a
Kevin Austin Holley <holley2032@gmail.com>
2020-10-26 15:27:13 -0400

    Some styling improvements.

--- client/src/components/AddList.js
+++ client/src/components/AddList.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, Modal, IconButton, Typography, Button, TextField } from "@material-ui/core";
+import {
+  Box,
+  Modal,
+  IconButton,
+  Typography,
+  Button,
+  TextField,
+} from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
 
 const useStyles = makeStyles((theme) => ({
-  '@global': {
-    '*::-webkit-scrollbar': {
-      width: '0.2rem'
+  "@global": {
+    "*::-webkit-scrollbar": {
+      width: "0.2rem",
     },
-    '*::-webkit-scrollbar-track': {
-      '-webkit-box-shadow': 'inset 0 0 6px rgba(10,10,10,0.1)'
+    "*::-webkit-scrollbar-track": {
+      "-webkit-box-shadow": "inset 0 0 6px rgba(10,10,10,0.1)",
     },
-    '*::-webkit-scrollbar-thumb': {
-      backgroundColor: 'rgba(1,1,1)',
-      outline: '1px solid slategrey'
-    }
+    "*::-webkit-scrollbar-thumb": {
+      backgroundColor: "rgba(1,1,1)",
+      outline: "1px solid slategrey",
+    },
   },
   popup: {
     display: "flex",
@@ -24,20 +31,17 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     position: "absolute",
     display: "flex",
-    //display:'block',
     flexDirection: "column",
     alignItems: "center",
-    //backgroundColor: theme.palette.background.paper,
     backgroundColor: "#f8f8f8",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
-    //padding: theme.spacing(1, 1, 3),
     borderRadius: "0.3rem",
     overflow: "hidden",
     outline: "none",
-    minWidth:"30rem",
+    minWidth: "30rem",
     maxHeight: "41rem",
-    height:'75%',
+    height: "75%",
   },
   addButton: {
     borderRadius: "10rem",
@@ -47,185 +51,195 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(2),
     width: "11rem",
     height: "3.5rem",
-    fontSize: "0.8rem"
+    fontSize: "0.8rem",
   },
   titleContainer: {
     display: "flex",
-    //padding: theme.spacing(0, 0, 3),
-    maxHeight:"3rem"
+    maxHeight: "3rem",
   },
   closeButtonContainer: {
     textAlign: "right",
-    width: "100%"
+    width: "100%",
   },
   titleText: {
     fontSize: "1.5rem",
-    fontWeight:"bold"
+    fontWeight: "bold",
   },
-  itemText:{
+  itemText: {
     fontSize: "0.8rem",
     fontWeight: "400",
-    letterSpacing:"0.5px"
+    letterSpacing: "0.5px",
   },
   productCard: {
     display: "flex",
     flexDirection: "row",
-    maxWidth: "30rem"
+    maxWidth: "30rem",
   },
-  removeButtonContainer:{
+  removeButtonContainer: {
     display: "flex",
-    alignItems:"center",
-    justifyContent:"center",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  removeButton:{
+  removeButton: {
     display: "flex",
-    alignItems:"center",
-    justifyContent:"center",
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: "10rem",
-    height:"3rem",
+    height: "3rem",
   },
-  addButtonContainer:{
+  addButtonContainer: {
     display: "flex",
-    justifyContent:"center",
-    margin:theme.spacing(3,0,2)
+    justifyContent: "center",
+    margin: theme.spacing(3, 0, 2),
   },
-  bodyContainer:{
-    minWidth:"30rem",
+  bodyContainer: {
+    minWidth: "30rem",
     maxHeight: "29rem",
-    overflow:"hidden",
-    display:"flex",
-    justifyContent:"center"
+    overflow: "hidden",
+    display: "flex",
+    justifyContent: "center",
+    width: "80%",
+    margin: theme.spacing(2),
   },
-  bodyContent:{
-    minWidth:"30rem",
+  bodyContent: {
+    minWidth: "30rem",
     maxHeight: "30rem",
-    overflow:"scroll",
-    overflowX:"hidden",
+    overflow: "scroll",
+    overflowX: "hidden",
   },
-  textFieldContainer:{
-    display:"flex",
-    justifyContent:"center",
-    width:"80%",
-    backgroundColor:"white"
+  textFieldContainer: {
+    display: "flex",
+    justifyContent: "center",
+    width: "80%",
+    backgroundColor: "white",
   },
-  centerText:{
-    textAlign:"center"
+  centerText: {
+    textAlign: "center",
   },
-  imageFieldContainer:{
-    display:"flex",
-    backgroundColor:"white",
-    width:"35%",
-    height:"0",
-    paddingBottom:"35%"
+  imageFieldContainer: {
+    display: "flex",
+    backgroundColor: "white",
+    width: "13rem",
+    height: "13rem",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    border: "1px dashed grey",
   },
   input: {
-    display:'none'
+    display: "none",
   },
 }));
 
 const AddList = (props) => {
-    const {addListOpen, setAddListOpen} = props;
-    const classes = useStyles()
-    const [errors, setErrors]=useState({});
-    const [title, setTitle]=useState('');
+  const { addListOpen, setAddListOpen } = props;
+  const classes = useStyles();
+  const [errors, setErrors] = useState({});
+  const [title, setTitle] = useState("");
 
-    const handleClose = () => {
-        setAddListOpen(false);
+  const handleClose = () => {
+    setAddListOpen(false);
+  };
+
+  const validations = () => {
+    const errorsCopy = { ...errors };
+    errorsCopy.title = title ? "" : "This field is required.";
+    setErrors({ ...errorsCopy });
+
+    return Object.values(errorsCopy).every((field) => field === "");
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (validations()) {
+      fetch("route", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: title,
+          //'cover': cover
+        }),
+      })
+        .then((response) => response.json())
+        .then(function (response) {
+          if (response.status === "success") {
+            console.log("Success:");
+          } else {
+            window.alert(response.message); // Replace with snackbar.
+            console.log(response.message);
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
     }
+  };
 
-    const validations = () => {
-        const errorsCopy = {...errors};
-        errorsCopy.title = title ? "" : "This field is required.";
-        setErrors({ ...errorsCopy });
-
-        return Object.values(errorsCopy).every(field => field === "");
-    }
-
-    const handleClick = (e) => {
-        e.preventDefault();
-        if (validations()) {
-            fetch("route", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    'title': title,
-                    //'cover': cover
-                })})
-                .then(response => response.json())
-                .then(function(response) {
-                    if (response.status === 'success') {
-                        console.log('Success:');
-                    }
-                    else {
-                        window.alert(response.message); // Replace with snackbar.
-                        console.log(response.message);
-                    }
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                })
-            }
-    }
-
-    return (
-        <Modal
-        className={classes.popup}
-        open={addListOpen}
-        onClose={handleClose}
-        aria-labelledby="add-list"
-        aria-describedby="confirms-adding-a-new-list"
-        >
-        <Box className={classes.paper}>
-          <Box className={classes.closeButtonContainer}>
-            <IconButton
-              aria-label="close"
-              className={classes.closeButton}
-              onClick={handleClose}
-              size="small"
-            >
-              <CloseIcon fontSize="small" />
-            </IconButton>
-          </Box>
-          <Box className={classes.titleContainer}>
-            <Typography><h2 className={classes.titleText}>Create new list</h2></Typography>
-          </Box>
-          <Box className={classes.bodyContainer}>
-            <Typography><h4 className={classes.itemText}>Add a title</h4></Typography>
-          </Box>
-          <Box className={classes.textFieldContainer}>
-            <TextField
-              className={classes.textField}
-              variant="outlined"
-              placeholder="Enter name" // Placeholder needs to be centered.
-              fullWidth
-              type="text"
-              error={!!errors.title}
-              helperText={errors.title}
-              onChange={(e)=>setTitle(e.target.value)}/>
-          </Box>
-          <Box className={classes.bodyContainer}>
-            <Typography><h4 className={classes.itemText}>Add a cover</h4></Typography>
-          </Box>
-          <Box className={classes.imageFieldContainer}>
-            <Typography>Drop an image here or <strong><u><input
-              accept="image/*"
-              className={classes.input}
-              id="file-input"
-              multiple
-              type="file"
-              />
-            <label htmlFor="file-input">
-              select a file
-            </label></u></strong></Typography>
-          </Box>
-          <Box className={classes.addButtonContainer}>
-            <Button className={classes.addButton} variant="contained">CREATE LIST</Button>
-          </Box>
+  return (
+    <Modal
+      className={classes.popup}
+      open={addListOpen}
+      onClose={handleClose}
+      aria-labelledby="add-list"
+      aria-describedby="confirms-adding-a-new-list"
+    >
+      <Box className={classes.paper}>
+        <Box className={classes.closeButtonContainer}>
+          <IconButton
+            aria-label="close"
+            className={classes.closeButton}
+            onClick={handleClose}
+            size="small"
+          >
+            <CloseIcon fontSize="small" />
+          </IconButton>
         </Box>
-        </Modal>
-    );
+        <Box className={classes.titleContainer}>
+          <Typography className={classes.titleText}>Create new list</Typography>
+        </Box>
+        <Box className={classes.bodyContainer}>
+          <Typography className={classes.itemText}>Add a title</Typography>
+          <TextField
+            className={classes.textField}
+            variant="outlined"
+            placeholder="Enter name" // Placeholder needs to be centered.
+            fullWidth
+            type="text"
+            error={!!errors.title}
+            helperText={errors.title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </Box>
+        <Box className={classes.bodyContainer}>
+          <Typography className={classes.itemText}>Add a cover</Typography>
+        </Box>
+        <Box className={classes.imageFieldContainer}>
+          <Typography>
+            Drop an image here or{" "}
+            <strong>
+              <u>
+                <input
+                  accept="image/*"
+                  className={classes.input}
+                  id="file-input"
+                  multiple
+                  type="file"
+                />
+                <label htmlFor="file-input">select a file</label>
+              </u>
+            </strong>
+          </Typography>
+        </Box>
+        <Box className={classes.addButtonContainer}>
+          <Button className={classes.addButton} variant="contained">
+            CREATE LIST
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  );
 };
 
 export default AddList;
--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, Modal, IconButton, Typography, Button, TextField, FormControl, InputLabel, MenuItem, Select } from "@material-ui/core";
+import {
+  Box,
+  Modal,
+  IconButton,
+  Typography,
+  Button,
+  TextField,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
 
 const useStyles = makeStyles((theme) => ({
-  '@global': {
-    '*::-webkit-scrollbar': {
-      width: '0.2rem'
+  "@global": {
+    "*::-webkit-scrollbar": {
+      width: "0.2rem",
     },
-    '*::-webkit-scrollbar-track': {
-      '-webkit-box-shadow': 'inset 0 0 6px rgba(10,10,10,0.1)'
+    "*::-webkit-scrollbar-track": {
+      "-webkit-box-shadow": "inset 0 0 6px rgba(10,10,10,0.1)",
     },
-    '*::-webkit-scrollbar-thumb': {
-      backgroundColor: 'rgba(1,1,1)',
-      outline: '1px solid slategrey'
-    }
+    "*::-webkit-scrollbar-thumb": {
+      backgroundColor: "rgba(1,1,1)",
+      outline: "1px solid slategrey",
+    },
   },
   popup: {
     display: "flex",
@@ -24,20 +35,17 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     position: "absolute",
     display: "flex",
-    //display:'block',
     flexDirection: "column",
     alignItems: "center",
-    //backgroundColor: theme.palette.background.paper,
     backgroundColor: "#f8f8f8",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
-    //padding: theme.spacing(1, 1, 3),
     borderRadius: "0.3rem",
     overflow: "hidden",
     outline: "none",
-    minWidth:"30rem",
+    minWidth: "30rem",
     maxHeight: "41rem",
-    height:'75%',
+    height: "75%",
   },
   addButton: {
     borderRadius: "10rem",
@@ -47,76 +55,75 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(2),
     width: "11rem",
     height: "3.5rem",
-    fontSize: "0.8rem"
+    fontSize: "0.8rem",
   },
   titleContainer: {
     display: "flex",
-    //padding: theme.spacing(0, 0, 3),
-    maxHeight:"3rem"
+    maxHeight: "3rem",
   },
   closeButtonContainer: {
     textAlign: "right",
-    width: "100%"
+    width: "100%",
   },
   titleText: {
     fontSize: "1.5rem",
-    fontWeight:"bold"
+    fontWeight: "bold",
   },
-  itemText:{
+  itemText: {
     fontSize: "0.8rem",
     fontWeight: "400",
-    letterSpacing:"0.5px"
+    letterSpacing: "0.5px",
   },
   productCard: {
     display: "flex",
     flexDirection: "row",
-    maxWidth: "30rem"
+    maxWidth: "30rem",
   },
-  removeButtonContainer:{
+  removeButtonContainer: {
     display: "flex",
-    alignItems:"center",
-    justifyContent:"center",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  removeButton:{
+  removeButton: {
     display: "flex",
-    alignItems:"center",
-    justifyContent:"center",
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: "10rem",
-    height:"3rem",
+    height: "3rem",
   },
-  addButtonContainer:{
+  addButtonContainer: {
     display: "flex",
-    justifyContent:"center",
-    margin:theme.spacing(3,0,2)
+    justifyContent: "center",
+    margin: theme.spacing(3, 0, 2),
   },
-  bodyContainer:{
-    minWidth:"30rem",
+  bodyContainer: {
+    minWidth: "30rem",
     maxHeight: "29rem",
-    overflow:"hidden",
-    display:"flex",
-    justifyContent:"center"
+    overflow: "hidden",
+    display: "flex",
+    justifyContent: "center",
   },
-  bodyContent:{
-    minWidth:"30rem",
+  bodyContent: {
+    minWidth: "30rem",
     maxHeight: "30rem",
-    overflow:"scroll",
-    overflowX:"hidden",
+    overflow: "scroll",
+    overflowX: "hidden",
   },
-  textFieldContainer:{
-    display:"flex",
-    justifyContent:"center",
-    width:"80%",
-    backgroundColor:"white"
+  textFieldContainer: {
+    display: "flex",
+    justifyContent: "center",
+    width: "80%",
+    backgroundColor: "white",
   },
-  centerText:{
-    textAlign:"center"
+  centerText: {
+    textAlign: "center",
   },
-  imageFieldContainer:{
-    display:"flex",
-    backgroundColor:"white",
-    width:"35%",
-    height:"0",
-    paddingBottom:"35%"
+  imageFieldContainer: {
+    display: "flex",
+    backgroundColor: "white",
+    width: "35%",
+    height: "0",
+    paddingBottom: "35%",
   },
   selectListInput: {
     display: "flex",
@@ -129,102 +136,116 @@ const useStyles = makeStyles((theme) => ({
     height: "4rem",
     padding: "0 0.5rem",
     boxShadow: "0 0 0.5rem 1px #eee",
-    width: "80%"
-  }
+    width: "80%",
+  },
 }));
 
 const AddList = (props) => {
-    const {listTitle, listOpen, changeListOpen, addProductOpen, changeAddProductOpen, demoListsArray} = props;
-    const classes = useStyles()
-    const [title, setTitle]=useState('');
-    const [list, setList]=useState('');
-    const [errors, setErrors]=useState({});
+  const {
+    listTitle,
+    listOpen,
+    changeListOpen,
+    addProductOpen,
+    changeAddProductOpen,
+    demoListsArray,
+  } = props;
+  const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [list, setList] = useState("");
+  const [errors, setErrors] = useState({});
 
-    const handleChange = (event) => {
-      setList(event.target.value);
-    };
+  const handleChange = (event) => {
+    setList(event.target.value);
+  };
 
-    const handleClose = () => {
-        changeAddProductOpen();
+  const handleClose = () => {
+    changeAddProductOpen();
+  };
+
+  const validations = () => {
+    const errorsCopy = { ...errors };
+    errorsCopy.title = title ? "" : "This field is required.";
+    setErrors({ ...errorsCopy });
+
+    return Object.values(errorsCopy).every((field) => field === "");
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (validations()) {
+      fetch("route", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: title,
+          //'cover': cover
+        }),
+      })
+        .then((response) => response.json())
+        .then(function (response) {
+          if (response.status === "success") {
+            console.log("Success:");
+          } else {
+            window.alert(response.message); // Replace with snackbar.
+            console.log(response.message);
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
     }
+  };
 
-    const validations = () => {
-        const errorsCopy = {...errors};
-        errorsCopy.title = title ? "" : "This field is required.";
-        setErrors({ ...errorsCopy });
-
-        return Object.values(errorsCopy).every(field => field === "");
-    }
-
-    const handleClick = (e) => {
-        e.preventDefault();
-        if (validations()) {
-            fetch("route", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    'title': title,
-                    //'cover': cover
-                })})
-                .then(response => response.json())
-                .then(function(response) {
-                    if (response.status === 'success') {
-                        console.log('Success:');
-                    }
-                    else {
-                        window.alert(response.message); // Replace with snackbar.
-                        console.log(response.message);
-                    }
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                })
-            }
-    }
-
-    return (
-        <Modal
-        className={classes.popup}
-        open={addProductOpen}
-        onClose={handleClose}
-        aria-labelledby="add-product"
-        aria-describedby="confirms-adding-item-of-url-to-selected-list"
-        >
-        <Box className={classes.paper}>
-          <Box className={classes.closeButtonContainer}>
-            <IconButton
-              aria-label="close"
-              className={classes.closeButton}
-              onClick={handleClose}
-              size="small"
-            >
-              <CloseIcon fontSize="small" />
-            </IconButton>
-          </Box>
-          <Box className={classes.titleContainer}>
-            <Typography className={classes.titleText}>Add new item:</Typography>
-          </Box>
-          <Box className={classes.bodyContainer}>
-            <Typography className={classes.itemText}>Paste link to item:</Typography>
-          </Box>
-          <Box className={classes.textFieldContainer}>
-            <TextField
-              className={classes.textField}
-              variant="outlined"
-              placeholder="Paste your link here" // Placeholder needs to be centered.
-              fullWidth
-              type="text"
-              error={!!errors.title}
-              helperText={errors.title}
-              onChange={(e)=>setTitle(e.target.value)}/>
-          </Box>
-          <Box className={classes.bodyContainer}>
-            <Typography className={classes.itemText}>Select list</Typography>
-          </Box>
-          <Box className={classes.selectListInput}>
-          <FormControl variant="outlined" fullWidth className={classes.formControl}>
+  return (
+    <Modal
+      className={classes.popup}
+      open={addProductOpen}
+      onClose={handleClose}
+      aria-labelledby="add-product"
+      aria-describedby="confirms-adding-item-of-url-to-selected-list"
+    >
+      <Box className={classes.paper}>
+        <Box className={classes.closeButtonContainer}>
+          <IconButton
+            aria-label="close"
+            className={classes.closeButton}
+            onClick={handleClose}
+            size="small"
+          >
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        </Box>
+        <Box className={classes.titleContainer}>
+          <Typography className={classes.titleText}>Add new item:</Typography>
+        </Box>
+        <Box className={classes.bodyContainer}>
+          <Typography className={classes.itemText}>
+            Paste link to item:
+          </Typography>
+        </Box>
+        <Box className={classes.textFieldContainer}>
+          <TextField
+            className={classes.textField}
+            variant="outlined"
+            placeholder="Paste your link here" // Placeholder needs to be centered.
+            fullWidth
+            type="text"
+            error={!!errors.title}
+            helperText={errors.title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </Box>
+        <Box className={classes.bodyContainer}>
+          <Typography className={classes.itemText}>Select list</Typography>
+        </Box>
+        <Box className={classes.selectListInput}>
+          <FormControl
+            variant="outlined"
+            fullWidth
+            className={classes.formControl}
+          >
             <InputLabel id="select-list-label">Select</InputLabel>
             <Select
               id="select-list"
@@ -233,17 +254,21 @@ const AddList = (props) => {
               label="List"
             >
               {demoListsArray.map((list, i) => (
-                <MenuItem key={i} value={list.listTitle}>{list.listTitle}</MenuItem>
+                <MenuItem key={i} value={list.listTitle}>
+                  {list.listTitle}
+                </MenuItem>
               ))}
             </Select>
           </FormControl>
-          </Box>
-          <Box className={classes.addButtonContainer}>
-            <Button className={classes.addButton} variant="contained">ADD ITEM</Button>
-          </Box>
         </Box>
-        </Modal>
-    );
+        <Box className={classes.addButtonContainer}>
+          <Button className={classes.addButton} variant="contained">
+            ADD ITEM
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  );
 };
 
 export default AddList;
