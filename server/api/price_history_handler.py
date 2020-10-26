1d68c90e831431b982248f130569dfd61ca6c182
Sam <samuel_morgan_@outlook.com>
2020-10-26 19:53:04 +0000

    Adds item to data (without price)

--- server/api/price_history_handler.py
+++ server/api/price_history_handler.py
@@ -1,6 +1,9 @@
 import json, re
 from flask import jsonify, request, Blueprint
+from models.prices import Prices
+from database import db
 price_history_handler = Blueprint('price_history_handler', __name__)
+from .scraper import ScrapeAmazon
 
 
 def get_url_id(URL):
@@ -9,12 +12,33 @@ def get_url_id(URL):
     
 
 @price_history_handler.route('/price_history', methods = ['GET', 'POST'])
-def scraped_items():
+def price_history():
     if request.method == 'GET':
-        return jsonify({'message' : 'Price History List'})
+        product_types = Prices.query.all()
+        product_types_ls = []
+        for item in product_types:
+            product_types_ls.append(
+                {
+                    "id": item.id,
+                    "url_id": item.url_id,
+                    "price": item.price,
+                    "date": item.date
+                })
+        return jsonify({'prices': product_types_ls}), 200
     if request.method == 'POST':
-        URL = json.loads(request.get_data())['URL']
-        print(get_url_id(URL))
-        return jsonify({'message' : 'Price History List'})
+        URL = json.loads(request.get_data())['url']
+        try:
+            item = ScrapeAmazon(URL)
+            url_id = get_url_id(URL)
+            price_entry = Prices(url_id, 0)
+            db.session.add(price_entry)
+        except:
+            return jsonify({'error': "{}".format(e.__cause__)}), 400      
+        try:
+            db.session.commit()
+        except Exception as e:
+            return jsonify({'error': "{}".format(e.__cause__)}), 400
+        return jsonify({'response': "{} was successfully added to the database".format(URL)}), 200
+
 
 
---
+++ server/models/prices.py
@@ -0,0 +1,16 @@
+from database import db
+import datetime
+
+class Prices(db.Model):
+
+    __tablename__ = 'price_history'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    url_id = db.Column(db.String, unique=True, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, url_id, price):
+        self.url_id = url_id
+        self.price = price
+        self.date = datetime.datetime.now(tz=datetime.timezone.utc)
