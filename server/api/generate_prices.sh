702368acb3f32d35ac99efd67e86066c47f2d6d3
Sam <samuel_morgan_@outlook.com>
2020-10-28 20:02:24 +0000

    Fixed bash script and Price uses ForeignKey

--- server/api/generate_prices.sh
+++ server/api/generate_prices.sh
@@ -5,5 +5,5 @@ for i in {1..10}
 do
     id=$(( ( RANDOM % 5 )  + 1 ))
     price=$(( RANDOM ))
-    curl -X POST localhost:5000/prices --data "{\"product_id\": ${id}, \"price\": ${price}}"  --header "Content-Type: application/json"
+    curl -X POST "localhost:5000/prices/product/${id}" --data "{\"price\": ${price}}"  --header "Content-Type: application/json"
 done
--- server/models/price.py
+++ server/models/price.py
@@ -1,12 +1,18 @@
 from database import db
 import datetime
 
+from .product import Product
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+
 class Price(db.Model):
 
     __tablename__ = 'prices'
 
+    product = relationship('Product')
+
     primary_id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
-    product_id = db.Column(db.Integer, nullable=False)
+    product_id = db.Column(ForeignKey('product.id'), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     scrape_date = db.Column(db.DateTime, nullable=False)
 
