f15613011e468ded8e270491af1f88be80499e6b
Sam <samuel_morgan_@outlook.com>
2020-10-29 14:12:30 +0000

    Tidied up try/except fpr prices routes

--- server/api/prices_handler.py
+++ server/api/prices_handler.py
@@ -10,36 +10,38 @@ from .scraper import ScrapeAmazon
 @prices_handler.route('/prices/product/<product_id>', methods=['GET', 'POST', 'DELETE'])
 def onePriceRequests(product_id):
     if request.method == 'GET':
-        prices = Price.query.filter_by(product_id=product_id)
-        return jsonify([price.serialize for price in prices]), 200
-
-    if request.method == 'POST':
-        body = json.loads(request.get_data())
         try:
-            price_entry = Price(product_id, body['price'])
-            db.session.add(price_entry)
-        except:
-            return jsonify({'error': "{}".format(e.__cause__)}), 400      
-        try:
-            db.session.commit()
+            prices = Price.query.filter_by(product_id=product_id)
+            return jsonify([price.serialize for price in prices]), 200
         except Exception as e:
             return jsonify({'error': "{}".format(e.__cause__)}), 400
-        return jsonify({'response': "Added price {} to product '{}'".format(body['price'], product_id)}), 200
+
+    if request.method == 'POST':
+        try:
+            body = json.loads(request.get_data())
+            price_entry = Price(product_id, body['price'])
+            db.session.add(price_entry)
+            db.session.commit()
+            return jsonify({'response': "Added price {} to product '{}'".format(body['price'], product_id)}), 200
+        except Exception as e:
+            return jsonify({'error': "{}".format(e.__cause__)}), 400
 
     if request.method == 'DELETE':
-        prices = Price.query.filter_by(product_id=product_id)
-        [db.session.delete(price) for price in prices]    
         try:
+            prices = Price.query.filter_by(product_id=product_id)
+            [db.session.delete(price) for price in prices]    
             db.session.commit()
+            return jsonify({'response': "Product '{}' prices were successfully deleted from the database".format(product_id)}), 200
         except Exception as e:
             return jsonify({'error': "{}".format(e.__cause__)}), 400    
-        return jsonify({'response': "Product '{}' prices were successfully deleted from the database".format(product_id)}), 200
     
 
 @prices_handler.route('/prices', methods = ['GET'])
 def allPriceRequests():
     if request.method == 'GET':
-        prices = Price.query.all()
-        return jsonify({'prices': [price.serialize for price in prices]}), 200
-
+        try:
+            prices = Price.query.all()
+            return jsonify({'prices': [price.serialize for price in prices]}), 200
+        except Exception as e:
+            return jsonify({'error': "{}".format(e.__cause__)}), 400    
 
