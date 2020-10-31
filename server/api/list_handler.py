25e2c04c320784f297f4fb4587a86d163341e599
Sam <samuel_morgan_@outlook.com>
2020-10-31 23:55:41 +0000

    Added PUT route to Lists

--- server/api/list_handler.py
+++ server/api/list_handler.py
@@ -7,24 +7,35 @@ import json
 list_handler = Blueprint('list_handler', __name__)
 
 
-@list_handler.route('/lists/<id>', methods=['GET', 'DELETE'])
+@list_handler.route('/lists/<id>', methods=['GET', 'DELETE', 'PUT'])
 def oneListRequests(id):
     if request.method == 'GET':
         try:
-            selected_list = List.query.get(id)
+            selected_list = List.query.get(int(id))
             return jsonify(selected_list.serialize), 200
         except Exception as e:
             return jsonify({'error': "{}".format(e.__cause__)}), 400
 
     if request.method == 'DELETE':
         try:        
-            selected_list = List.query.get(id)
+            selected_list = List.query.get(int(id))
             db.session.delete(selected_list)
             db.session.commit()
             return jsonify({'response': "List '{}' was successfully deleted from the database".format(id)}), 200
         except Exception as e:
             return jsonify({'error': "{}".format(e.__cause__)}), 400    
 
+    if request.method == "PUT":
+        try:
+            body = json.loads(request.get_data())
+            selected_list = List.query.get(int(id))     
+            if selected_list:       
+                if "product_type" in body: selected_list.product_type = body["product_type"]
+                if "visible" in body: selected_list.visible = body["visible"]
+                db.session.commit()
+                return jsonify({"response": "Product '{}' was updated".format(id)}), 200
+        except Exception as e:
+            return jsonify({'error': "{}".format(e.__cause__)}), 400
 
 @list_handler.route('/lists', methods=['GET', 'POST'])
 def allListRequests():
