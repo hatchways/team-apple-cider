a7dfc074c7c7fd2adb8fba2d483bf55ee9f9ebae
Sam <samuel_morgan_@outlook.com>
2020-11-01 22:53:56 +0000

    Created profile model

--- server/models/list.py
+++ server/models/list.py
@@ -4,7 +4,7 @@ class List(db.Model):
 
     __tablename__ = 'list'
 
-    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     product_type = db.Column(db.String, unique=True, nullable=False)
     visible = db.Column(db.Boolean, nullable=False)
     # have an image type aswell, will implement later...
---
+++ server/models/profile.py
@@ -0,0 +1,25 @@
+from database import db
+
+from .user import User
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey 
+
+class Profile(db.Model):
+
+    __tablename__ = 'profile'
+
+    id = db.Column(ForeignKey('user.id'), primary_key=True, nullable=False) 
+    name = db.Column(db.String, nullable=False)
+    photo = db.Column(db.String)
+
+    def __init__(self, name, photo):
+        self.name = name
+        self.photo = photo
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            "name": self.name,
+            "photo": self.photo,
+        }
