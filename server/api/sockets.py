e83c40c9dd44e724d46dbcd5ad50f2c71e15e33d
Sam <samuel_morgan_@outlook.com>
2020-11-04 20:22:31 +0000

    Changed print wording

--- server/api/sockets.py
+++ server/api/sockets.py
@@ -3,5 +3,5 @@ from flask_socketio import SocketIO, emit
 def attach_events(socketio):
     @socketio.on("connect")
     def handle_message():
-        print("I'm connected")
+        print("Server socket connected")
         emit("connection_message", "Socket.io connection established")
