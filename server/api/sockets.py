9e2be131a37c6fb4af50a118ca6aff8c134c2d56
Sam <samuel_morgan_@outlook.com>
2020-11-04 20:19:23 +0000

    Changed connection message

--- server/api/sockets.py
+++ server/api/sockets.py
@@ -4,4 +4,4 @@ def attach_events(socketio):
     @socketio.on("connect")
     def handle_message():
         print("I'm connected")
-        emit("someEvent", "OKOKOK")
+        emit("Message", "Socket.io connection established")
