fda270fd42aa4c44fbd3718c0d1a9a34863e2436
Sam <samuel_morgan_@outlook.com>
2020-11-04 20:11:23 +0000

    if __name__ == "__main__" comparison

--- server/app.py
+++ server/app.py
@@ -12,12 +12,13 @@ from api.prices_handler import prices_handler
 from api.profile_handler import profile_handler 
 
 
-from api.sockets import SocketIO, attach_events
 import eventlet
+from api.sockets import SocketIO, attach_events
 
 socketio = SocketIO()
 
 def create_app(): 
+    print(__name__)
     app = Flask(__name__)
     app.config.from_object('config.DevelopmentConfig')
 
@@ -26,9 +27,10 @@ def create_app():
     migrate.init_app(app, db)
     cors.init_app(app)
     flask_bcrypt.init_app(app)
-    
+
     socketio.init_app(app)
-    socketio.run(app)
+    if __name__ == "__main__":
+        socketio.run(app)
     attach_events(socketio)
 
     with app.app_context():
