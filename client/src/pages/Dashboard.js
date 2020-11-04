01c172598cf5d4dbd06da27e2d3b3c5f52792c8f
Sam <samuel_morgan_@outlook.com>
2020-11-04 13:12:32 +0000

    Remove unused useEffect from dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Header from "components/Header";
