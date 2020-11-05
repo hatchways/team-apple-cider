ef5870761e64ac89b501c94b21059d0e8b5c01af
adams <adamsliu97@gmail.com>
2020-11-05 02:55:33 -0500

    added backend api handlers

--- server/api/follower_handler.py
+++ server/api/follower_handler.py
@@ -6,25 +6,61 @@ from api.auth_handler import token_getter
 follower_handler = Blueprint('follower_handler', __name__)
 
 
-@follower_handler.route('/followers/<user_id>' , methods=['GET'])
-def getFollowers():
-    pass
+@follower_handler.route('/followers/<user_id>', methods=['GET'])
+def getFollowers(user_id):
+    auth_token = token_getter()
+    try:
+        user = User.query.get(user_id)
+        followers = user.followers.all()
+        return jsonify([user.serialize for user in followers]), 200
+    except Exception as e:
+        return jsonify({'error': "{}".format(e.__cause__)}), 400
 
 
-@follower_handler.route('/following/<user_id>', methods=['GET'])
-def getFollowings():
-    pass
+@follower_handler.route('/followings/<user_id>', methods=['GET'])
+def getFollowings(user_id):
+    auth_token = token_getter()
+    try:
+        user = User.query.get(user_id)
+        followings = user.followed.all()
+        return jsonify([user.serialize for user in followings]), 200
+    except Exception as e:
+        return jsonify({'error': "{}".format(e.__cause__)}), 400
 
 
 @follower_handler.route('/follow/<user_id>', methods=['GET'])
-def followUser():
-    pass
-    
+def followUser(user_id):
+    auth_token = token_getter()
+    if type(auth_token) is not int:
+        return jsonify({"error": "must be signed in to follow users"}), 400
+    else:
+        if user_id == auth_token:
+            return jsonify({"error": "you can't follow yourself"}), 400
+        else:
+            try:
+                user = User.query.get(auth_token)
+                follow_user = User.query.get(user_id)
+                user.follow(follow_user)
+                db.session.commit()
+            except Exception as e:
+                return jsonify({'error': "{}".format(e.__cause__)}), 400
+            return jsonify({'response': "you've successfully followed user {}".format(user_id)}), 400
 
 
 @follower_handler.route('/unfollow/<user_id>', methods=['GET'])
-def unfollowUser():
-    pass
-
-
-    
+def unfollowUser(user_id):
+    auth_token = token_getter()
+    if type(auth_token) is not int:
+        return jsonify({"error": "must be signed in to unfollow users"}), 400
+    else:
+        if user_id == auth_token:
+            return jsonify({"error": "you can't unfollow yourself"}), 400
+        else:
+            try:
+                user = User.query.get(auth_token)
+                unfollow_user = User.query.get(user_id)
+                user.unfollow(unfollow_user)
+                db.session.commit()
+            except Exception as e:
+                return jsonify({'error': "{}".format(e.__cause__)}), 400
+            return jsonify({'response': "you've successfully unfollowed user {}".format(user_id)}), 400
--- server/models/user.py
+++ server/models/user.py
@@ -1,15 +1,19 @@
 from server import flask_bcrypt
 from database import db
+from flask_login import UserMixin
 from config import DevelopmentConfig
 import datetime
 import jwt
 
 followers = db.Table('followers',
-        db.Column('follower_id', db.Integer, db.ForeignKey('users.id')),
-        db.Column('followed_id', db.Integer, db.ForeignKey('users.id'))
-)
+                     db.Column('follower_id', db.Integer,
+                               db.ForeignKey('users.id')),
+                     db.Column('followed_id', db.Integer,
+                               db.ForeignKey('users.id'))
+                     )
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -17,16 +21,17 @@ class User(db.Model):
     name = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
     registered_time = db.Column(db.DateTime, nullable=False)
-    followed = db.relationship('User', secondary=followers, 
-    primaryjoin=(followers.c.follower_id == id), 
-    secondaryjoin=(followers.c.followed_id == id), 
-    backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    followed = db.relationship('User', secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __init__(self, email, name, password):
         self.email = email
         self.name = name
-        self.password = flask_bcrypt.generate_password_hash(password, DevelopmentConfig.BCRYPT_LOG_ROUNDS).decode()
-        self.registered_time = datetime.datetime.now(tz=datetime.timezone.utc)  
+        self.password = flask_bcrypt.generate_password_hash(
+            password, DevelopmentConfig.BCRYPT_LOG_ROUNDS).decode()
+        self.registered_time = datetime.datetime.now(tz=datetime.timezone.utc)
 
     def follow(self, user):
         if not self.is_following(user):
@@ -37,8 +42,9 @@ class User(db.Model):
             self.followed.remove(user)
 
     def is_following(self, user):
-        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
 
     def encode_auth_token(self, user_id):
         try:
@@ -65,3 +71,13 @@ class User(db.Model):
         except jwt.InvalidTokenError:
             return "Invalid token. Please log in again."
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "password": self.password,
+            "registered_time": self.registered_time
+        }
