93d16a294ae5af17b78d0e6078a754775127c7c2
Sam <samuel_morgan_@outlook.com>
2020-11-05 18:07:26 +0000

    Prints the time

--- server/api/celery_api/scrape_schedule.py
+++ server/api/celery_api/scrape_schedule.py
@@ -1,17 +1,17 @@
 from celery import Celery, signature
 from celery.schedules import crontab
 from api.scraper import ScrapeAmazon
+from datetime import datetime
 import requests
-
 
 app = Celery('scrape_schedule', backend='rpc://', broker='amqp://guest@localhost//')
 app.conf.timezone = ('US/Eastern')
 
 @app.task
 def scrape_url():
-
     product_req = requests.get('http://localhost:5000/products')
     items = product_req.json()
+    print('Scraping all products at {}'.format(datetime.now()))
 
     for item in items:
         URL = item['url']
@@ -24,7 +24,6 @@ def scheduled_tasks(sender, **kwargs):
     app.conf.beat_schedule = {
             "task_name": {
                 "task": "api.celery_api.scrape_schedule.scrape_url",
-                "schedule": 25.0,
-                # "args": ()
+                "schedule": 60.0
             }, 
         }
