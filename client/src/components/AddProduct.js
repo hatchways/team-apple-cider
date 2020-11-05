43acadaf2e25c684cfdc526359efdc22985c4b92
Kevin Austin Holley <holley2032@gmail.com>
2020-11-05 17:32:42 -0500

    Implemented scraping.

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState, useEffect, useContext } from "react";
 import {
   Box,
   Modal,
@@ -11,9 +10,12 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  CircularProgress,
 } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
 import useStyles from "../styles/AddProductStyles";
+import UserContext from "contexts/UserContext";
+import ItemDisplay from "components/ItemDisplay";
 
 const AddProduct = (props) => {
   const {
@@ -25,9 +27,54 @@ const AddProduct = (props) => {
     lists,
   } = props;
   const classes = useStyles();
-  const [url, setUrl] = useState("");
+  const [inputLink, setInputLink] = useState("");
   const [list, setList] = useState("");
   const [errors, setErrors] = useState({});
+  const userId = useContext(UserContext).userId;
+  const [item, setItem] = useState({});
+  const [selectedListIndex, setSelectedListIndex] = useState(0);
+  const [listId, setListId] = useState("");
+  const [userLists, setUserLists] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getItem = async (input) => {
+    const response = await fetch("/scrape", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ url: input }),
+    });
+    return response.json();
+  };
+
+  const getLists = async () => {
+    const res = await fetch(`/lists?user_id=${userId}`);
+    const json = await res.json();
+    setUserLists(json);
+  };
+
+  useEffect(() => {
+    getLists();
+  }, []);
+
+  const findButtonClick = async (e) => {
+    if (inputLink.length > 0) {
+      setLoading(true);
+      const newItem = await getItem(inputLink);
+      setItem(newItem);
+      setInputLink("");
+      setLoading(false);
+    }
+  };
+
+  const addButtonClick = (e) => {
+    e.preventDefault();
+  };
+
+  const onChangeList = (e) => {
+    const newIndex = parseInt(e.target.value);
+    setSelectedListIndex(newIndex);
+    setListId(userLists[newIndex].id);
+  };
 
   const handleChange = (event) => {
     setList(event.target.value);
@@ -39,56 +86,10 @@ const AddProduct = (props) => {
 
   const validations = () => {
     const errorsCopy = { ...errors };
-    errorsCopy.url = url ? "" : "This field is required.";
+    errorsCopy.inputLink = inputLink ? "" : "This field is required.";
     setErrors({ ...errorsCopy });
 
     return Object.values(errorsCopy).every((field) => field === "");
-  };
-
-  const handleClick = async (e) => {
-    e.preventDefault();
-    if (validations()) {
-      fetch("/scrape", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          url: url,
-        }),
-      })
-        .then((response) => response.json())
-        .then(function (response) {
-          if (response.status === "success") {
-            console.log("Success on scraping!");
-          } else {
-            console.log(response.message);
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-      fetch(`/list-to-products/${list.id}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          product_id: item.product_id,
-        }),
-      })
-        .then((response) => response.json())
-        .then(function (response) {
-          if (response.status === "success") {
-            console.log("Success:");
-          } else {
-            console.log(response.message);
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    }
   };
 
   return (
@@ -125,9 +126,9 @@ const AddProduct = (props) => {
             placeholder="Paste your link here" // Placeholder needs to be centered.
             fullWidth
             type="text"
-            error={Boolean(errors.url)}
-            helperText={errors.url}
-            onChange={(e) => setUrl(e.target.value)}
+            error={Boolean(errors.inputLink)}
+            helperText={errors.inputLink}
+            onChange={(e) => setInputLink(e.target.value)}
           />
         </Box>
         <Box className={classes.bodyContainer}>
@@ -154,15 +155,33 @@ const AddProduct = (props) => {
             </Select>
           </FormControl>
         </Box>
-        <Box className={classes.addButtonContainer}>
-          <Button
-            className={classes.addButton}
-            variant="contained"
-            onClick={handleClick}
-          >
-            ADD ITEM
-          </Button>
+        <Box className={classes.itemContainer}>
+          {item.error ? (
+            <Box className={classes.errorMessage}>{item.response}</Box>
+          ) : item.name ? (
+            <>
+              <ItemDisplay item={item} className={classes.itemDisplay} />,
+              <Button
+                onClick={addButtonClick}
+                variant="contained"
+                className={classes.addButton}
+              >
+                ADD ITEM
+              </Button>
+            </>
+          ) : loading ? (
+            <CircularProgress className={classes.spinner} />
+          ) : (
+            <Button
+              className={classes.addButton}
+              variant="contained"
+              onClick={findButtonClick}
+            >
+              FIND ITEM
+            </Button>
+          )}
         </Box>
+        <Box className={classes.addButtonContainer}></Box>
       </Box>
     </Modal>
   );
