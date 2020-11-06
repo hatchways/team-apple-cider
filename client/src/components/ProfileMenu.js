08d30b3564418ee4f394b2f6738f2047c7e3fb0d
Sam <samuel_morgan_@outlook.com>
2020-11-06 16:42:57 +0000

    Uploading profile pics working

--- client/src/components/ProfileMenu.js
+++ client/src/components/ProfileMenu.js
@@ -1,8 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Box, Typography, Button, Menu, MenuItem } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { withRouter } from "react-router-dom";
-import icon from "img/icon.png";
+import Dropzone from "react-dropzone";
 
 import UserContext from "contexts/UserContext";
 
@@ -21,6 +21,7 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
     height: "3.5rem",
+    width: "3.5rem",
     borderRadius: "100%",
   },
   profileButtonText: {
@@ -30,6 +31,8 @@ const useStyles = makeStyles((theme) => ({
 
 const ProfileMenu = (props) => {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [imageURL, setImageURL] = useState("");
+  const [userIcon, setUserIcon] = useState(null);
   const classes = useStyles();
   const value = useContext(UserContext);
   const handleClick = (e) => setAnchorEl(e.currentTarget);
@@ -40,18 +43,63 @@ const ProfileMenu = (props) => {
     props.history.push("/");
   };
 
-  const handlePhotoClick = () => {
-    console.log("hiya");
+  const uploadProfilePicture = async () => {
+    const auth = await (await fetch("/auth/status")).json();
+    const userId = auth.data.user_id;
+    await fetch(`/profiles/${userId}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ photo: imageURL }),
+    });
+  };
+
+  const retrieveUserIcon = async () => {
+    const auth = await (await fetch("/auth/status")).json();
+    const userId = auth.data.user_id;
+    const user = await (await fetch(`/profiles/${userId}`)).json();
+    setUserIcon(user.photo);
+  };
+
+  useEffect(() => {
+    retrieveUserIcon();
+  }, []);
+
+  useEffect(() => {
+    const updateUserPhoto = async () => {
+      if (imageURL) {
+        await uploadProfilePicture();
+        retrieveUserIcon();
+      }
+    };
+    updateUserPhoto();
+  }, [imageURL]);
+
+  const onDrop = (acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+      reader.addEventListener(
+        "load",
+        function () {
+          // convert image file to base64 string
+          let src = reader.result;
+          setImageURL(src);
+        },
+        false
+      );
+      if (file) reader.readAsDataURL(file);
+    });
   };
 
   return (
     <Box className={classes.profileMenuContainer}>
-      <img
-        className={classes.profileIcon}
-        src={icon}
-        alt="profile"
-        onClick={handlePhotoClick}
-      />
+      <Dropzone onDrop={onDrop} accept="image/*">
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <img className={classes.profileIcon} src={userIcon} alt="profile" />
+          </div>
+        )}
+      </Dropzone>
       <Button onClick={handleClick}>
         <Typography className={classes.profileButtonText}>Profile</Typography>
       </Button>
