59b463d49b22cac4f5dd79509e1c32a134acc3f5
Sam <samuel_morgan_@outlook.com>
2020-11-17 14:13:02 +0000

    Prettier formatted

--- client/src/components/DeleteListPopup.js
+++ client/src/components/DeleteListPopup.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
 
 const DeleteListPopup = (props) => {
   const [open, setOpen] = useState(false);
   const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -32,11 +32,13 @@ const DeleteListPopup = (props) => {
         onClose={handleClose}
         aria-labelledby="responsive-dialog-title"
       >
-        <DialogTitle id="responsive-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogTitle id="responsive-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
         <DialogContent>
           <DialogContentText>
-            Let Google help apps determine location. This means sending anonymous location data to
-            Google, even when no apps are running.
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
           </DialogContentText>
         </DialogContent>
         <DialogActions>
@@ -50,6 +52,6 @@ const DeleteListPopup = (props) => {
       </Dialog>
     </div>
   );
-}
+};
 
 export default DeleteListPopup;
--- client/src/components/ListsDisplay.js
+++ client/src/components/ListsDisplay.js
@@ -12,140 +12,140 @@ import home from "img/default/home.png";
 import travel from "img/default/travel.png";
 
 const useStyles = makeStyles((theme) => ({
-    shoppingContainer: {
-        display: "flex",
-        flexDirection: "column",
+  shoppingContainer: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  myShoppingLists: {
+    display: "flex",
+    maxWidth: "75vw",
+    overflowX: "auto",
+    "& > *": {
+      marginRight: theme.spacing(4),
     },
-    myShoppingLists: {
-        display: "flex",
-        maxWidth: "75vw",
-        overflowX: "auto",
-        "& > *": {
-            marginRight: theme.spacing(4),
-        },
-        "& > *:last-child": {
-            marginRight: 0,
-        },
+    "& > *:last-child": {
+      marginRight: 0,
     },
+  },
 
-    addNewList: {
-        padding: theme.spacing(12),
-        backgroundColor: "white",
-        borderRadius: "1rem",
-        textAlign: "center",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    addNewListButton: {
-        height: "5rem",
-        width: "5rem",
-        margin: "0.5rem",
-    },
-    addIcon: {
-        color: "red",
-        height: "3rem",
-        width: "3rem",
-        margin: "0.5rem",
-    },
-    addNewListText: {
-        fontWeight: "bold",
-    },
+  addNewList: {
+    padding: theme.spacing(12),
+    backgroundColor: "white",
+    borderRadius: "1rem",
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  addNewListButton: {
+    height: "5rem",
+    width: "5rem",
+    margin: "0.5rem",
+  },
+  addIcon: {
+    color: "red",
+    height: "3rem",
+    width: "3rem",
+    margin: "0.5rem",
+  },
+  addNewListText: {
+    fontWeight: "bold",
+  },
 }));
 
 const defaultLists = [
-    {
-        name: "Home",
-        id: 2,
-        user_id: 6,
-        img_url: home,
-    },
-    {
-        name: "Fashion",
-        id: 2,
-        user_id: 6,
-        img_url: fashion,
-    },
-    {
-        name: "Travel",
-        id: 2,
-        user_id: 6,
-        img_url: travel,
-    },
+  {
+    name: "Home",
+    id: 2,
+    user_id: 6,
+    img_url: home,
+  },
+  {
+    name: "Fashion",
+    id: 2,
+    user_id: 6,
+    img_url: fashion,
+  },
+  {
+    name: "Travel",
+    id: 2,
+    user_id: 6,
+    img_url: travel,
+  },
 ];
 
 const ListsDisplay = (props) => {
-    const userLists = useContext(ListContext).lists;
-    const scrollRef = useHorizontalScroll();
-    const { profile, changeAddListOpen } = props;
-    const classes = useStyles();
-    const [lists, setLists] = useState(userLists);
-    const [snackText, setSnackText] = useState("");
-    const [openSuccessSnack, setOpenSuccessSnack] = useState(false);
+  const userLists = useContext(ListContext).lists;
+  const scrollRef = useHorizontalScroll();
+  const { profile, changeAddListOpen } = props;
+  const classes = useStyles();
+  const [lists, setLists] = useState(userLists);
+  const [snackText, setSnackText] = useState("");
+  const [openSuccessSnack, setOpenSuccessSnack] = useState(false);
 
-    const getLists = async (id) => {
-        const res = await fetch(`/lists?user_id=${id}`);
-        const json = await res.json();
-        return json;
-    };
+  const getLists = async (id) => {
+    const res = await fetch(`/lists?user_id=${id}`);
+    const json = await res.json();
+    return json;
+  };
 
-    const refreshLists = async () => {
-        if (profile) {
-            const profileLists = await getLists(profile.id);
-            setLists(profileLists);
-        } else if (userLists.length === 0) setLists(defaultLists);
-        else setLists(userLists);
-    };
+  const refreshLists = async () => {
+    if (profile) {
+      const profileLists = await getLists(profile.id);
+      setLists(profileLists);
+    } else if (userLists.length === 0) setLists(defaultLists);
+    else setLists(userLists);
+  };
 
-    useEffect(() => {
-        refreshLists();
-    }, [profile, userLists]);
+  useEffect(() => {
+    refreshLists();
+  }, [profile, userLists]);
 
-    const changeOpenSuccessSnack = (message) => {
-        setSnackText(message);
-        setOpenSuccessSnack(true);
-    };
+  const changeOpenSuccessSnack = (message) => {
+    setSnackText(message);
+    setOpenSuccessSnack(true);
+  };
 
-    const handleCloseSnack = (event, reason) => {
-        if (reason === "clickaway") return;
-        setOpenSuccessSnack(false);
-    };
+  const handleCloseSnack = (event, reason) => {
+    if (reason === "clickaway") return;
+    setOpenSuccessSnack(false);
+  };
 
-    return (
-        <Box className={`${classes.shoppingContainer} ${props.className}`}>
-            <ListsDisplayTitle {...{ profile, lists }} />
-            <Box className={classes.myShoppingLists} ref={scrollRef}>
-                {lists.length !== 0 &&
-                    lists.map((list, i) => (
-                        <ListCard
-                            key={i}
-                            list={list}
-                            lists={lists}
-                            changeOpenSuccessSnack={changeOpenSuccessSnack}
-                        />
-                    ))}
-                {!profile && (
-                    <Box className={classes.addNewList}>
-                        <IconButton
-                            className={classes.addNewListButton}
-                            onClick={() => changeAddListOpen()}
-                        >
-                            <AddIcon className={classes.addIcon} />
-                        </IconButton>
-                        <Typography className={classes.addNewListText}>
-                            ADD NEW LIST
-                        </Typography>
-                    </Box>
-                )}
-            </Box>
-            <SuccessSnackbar
-                openSnack={openSuccessSnack}
-                handleCloseSnack={handleCloseSnack}
-                snackText={snackText}
+  return (
+    <Box className={`${classes.shoppingContainer} ${props.className}`}>
+      <ListsDisplayTitle {...{ profile, lists }} />
+      <Box className={classes.myShoppingLists} ref={scrollRef}>
+        {lists.length !== 0 &&
+          lists.map((list, i) => (
+            <ListCard
+              key={i}
+              list={list}
+              lists={lists}
+              changeOpenSuccessSnack={changeOpenSuccessSnack}
             />
-        </Box>
-    );
+          ))}
+        {!profile && (
+          <Box className={classes.addNewList}>
+            <IconButton
+              className={classes.addNewListButton}
+              onClick={() => changeAddListOpen()}
+            >
+              <AddIcon className={classes.addIcon} />
+            </IconButton>
+            <Typography className={classes.addNewListText}>
+              ADD NEW LIST
+            </Typography>
+          </Box>
+        )}
+      </Box>
+      <SuccessSnackbar
+        openSnack={openSuccessSnack}
+        handleCloseSnack={handleCloseSnack}
+        snackText={snackText}
+      />
+    </Box>
+  );
 };
 
 export default ListsDisplay;
