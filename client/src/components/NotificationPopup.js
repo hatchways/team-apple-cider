b2311610abb9c127200859ed50ec335e3eabbed2
Sam <samuel_morgan_@outlook.com>
2020-11-27 16:39:13 +0000

    Added prettier settings

--- client/src/components/NotificationPopup.js
+++ client/src/components/NotificationPopup.js
@@ -3,50 +3,50 @@ import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  dashboardNotifications: {
-    left: "0",
-    top: "0",
-    transform: "translate(-50%,-22.9%)",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    margin: "4rem",
-    position: "absolute",
-    width: "12rem",
-  },
-  notificationsContainer: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    borderTop: "2px solid #DF1B1B",
-    backgroundColor: "white",
-    width: "100%",
-    padding: "1rem",
-    boxShadow: "0px 5px 10px 1px #f4f4f4",
-  },
-  arrow: {
-    width: "0",
-    height: "0",
-    borderStyle: "solid",
-    borderWidth: "0 6px 6px 6px",
-    borderColor: "transparent transparent #DF1B1B transparent",
-  },
+    dashboardNotifications: {
+        left: "0",
+        top: "0",
+        transform: "translate(-50%,-22.9%)",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        margin: "4rem",
+        position: "absolute",
+        width: "12rem"
+    },
+    notificationsContainer: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        borderTop: "2px solid #DF1B1B",
+        backgroundColor: "white",
+        width: "100%",
+        padding: "1rem",
+        boxShadow: "0px 5px 10px 1px #f4f4f4"
+    },
+    arrow: {
+        width: "0",
+        height: "0",
+        borderStyle: "solid",
+        borderWidth: "0 6px 6px 6px",
+        borderColor: "transparent transparent #DF1B1B transparent"
+    }
 }));
 
 const NotificationPopup = () => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Box className={classes.dashboardNotifications}>
-      <Box className={classes.arrow}></Box>
-      <Box className={classes.notificationsContainer}>
-        <Typography>New price!</Typography>
-        <Typography>New price!</Typography>
-        <Typography>New price!</Typography>
-        <Typography>New price!</Typography>
-      </Box>
-    </Box>
-  );
+    return (
+        <Box className={classes.dashboardNotifications}>
+            <Box className={classes.arrow}></Box>
+            <Box className={classes.notificationsContainer}>
+                <Typography>New price!</Typography>
+                <Typography>New price!</Typography>
+                <Typography>New price!</Typography>
+                <Typography>New price!</Typography>
+            </Box>
+        </Box>
+    );
 };
 
 export default NotificationPopup;
--- client/src/components/ProfileTopBar.js
+++ client/src/components/ProfileTopBar.js
@@ -4,92 +4,99 @@ import { makeStyles } from "@material-ui/core/styles";
 import FollowBackIcon from "@material-ui/icons/Autorenew";
 
 const useStyles = makeStyles((theme) => ({
-  profileTopBar: {
-    display: "flex",
-    alignItems: "center",
-  },
-  profilePhoto: {
-    borderRadius: "100%",
-    height: "7rem",
-    margin: theme.spacing(2, 2, 2, 2),
-  },
-  profileTextContainer: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "start",
-  },
-  userName: {
-    fontSize: "1.5rem",
-    fontWeight: "bold",
-    margin: theme.spacing(0.5),
-  },
-  followerBox: {
-    display: "flex",
-    alignItems: "center",
-  },
-  followerTextContainer: {
-    display: "flex",
-    width: "fit-content",
-    flexDirection: "column",
-    margin: theme.spacing(0, 0.75),
-  },
-  followerText: {
-    color: "grey",
-    fontSize: "0.7rem",
-  },
-  buttonContainer: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "7rem",
-  },
-  followButton: {
-    backgroundColor: "#DF1B1B",
-    color: "white",
-    width: "auto",
-    height: "2rem",
-    margin: theme.spacing(0.75, 1.25),
-  },
+    profileTopBar: {
+        display: "flex",
+        alignItems: "center"
+    },
+    profilePhoto: {
+        borderRadius: "100%",
+        height: "7rem",
+        margin: theme.spacing(2, 2, 2, 2)
+    },
+    profileTextContainer: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "start"
+    },
+    userName: {
+        fontSize: "1.5rem",
+        fontWeight: "bold",
+        margin: theme.spacing(0.5)
+    },
+    followerBox: {
+        display: "flex",
+        alignItems: "center"
+    },
+    followerTextContainer: {
+        display: "flex",
+        width: "fit-content",
+        flexDirection: "column",
+        margin: theme.spacing(0, 0.75)
+    },
+    followerText: {
+        color: "grey",
+        fontSize: "0.7rem"
+    },
+    buttonContainer: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "7rem"
+    },
+    followButton: {
+        backgroundColor: "#DF1B1B",
+        color: "white",
+        width: "auto",
+        height: "2rem",
+        margin: theme.spacing(0.75, 1.25)
+    }
 }));
 
 const ProfileTopBar = (props) => {
-  const { profile, toggleFollow, relation } = props;
-  const classes = useStyles();
-  return (
-    <Box className={classes.profileTopBar}>
-      <img
-        className={classes.profilePhoto}
-        src={profile && profile.photo}
-        alt="profile-pic"
-      />
-      <Box className={classes.profileTextContainer}>
-        <Typography className={classes.userName}>{profile.name}</Typography>
+    const { profile, toggleFollow, relation } = props;
+    const classes = useStyles();
+    return (
+        <Box className={classes.profileTopBar}>
+            <img
+                className={classes.profilePhoto}
+                src={profile && profile.photo}
+                alt="profile-pic"
+            />
+            <Box className={classes.profileTextContainer}>
+                <Typography className={classes.userName}>
+                    {profile.name}
+                </Typography>
 
-        <Box className={classes.followerBox}>
-          <Box className={classes.followerTextContainer}>
-            <Typography className={classes.followerText}>
-              {profile.follower_count} Followers
-            </Typography>
-            <Typography className={classes.followerText}>
-              {profile.following_count} Following
-            </Typography>
-          </Box>
-          <Box className={classes.buttonContainer}>
-            <Button className={classes.followButton} onClick={toggleFollow}>
-              {relation.following ? "unfollow" : "follow"}
-            </Button>
-          </Box>
-          {relation.following && relation.follows_back && (
-            <Tooltip
-              title={`You and ${profile.name} are following each other!`}
-            >
-              <FollowBackIcon className={classes.followBackIcon} />
-            </Tooltip>
-          )}
+                <Box className={classes.followerBox}>
+                    <Box className={classes.followerTextContainer}>
+                        <Typography className={classes.followerText}>
+                            {profile.follower_count} Followers
+                        </Typography>
+                        <Typography className={classes.followerText}>
+                            {profile.following_count} Following
+                        </Typography>
+                    </Box>
+                    <Box className={classes.buttonContainer}>
+                        <Button
+                            className={classes.followButton}
+                            onClick={toggleFollow}
+                        >
+                            {relation.following ? "unfollow" : "follow"}
+                        </Button>
+                    </Box>
+                    {relation.following && relation.follows_back && (
+                        <Tooltip
+                            title={`You and ${profile.name} are following each other!`}
+                        >
+                            <FollowBackIcon
+                                className={classes.followBackIcon}
+                            />
+                        </Tooltip>
+                    )}
+                </Box>
+            </Box>
         </Box>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default ProfileTopBar;
